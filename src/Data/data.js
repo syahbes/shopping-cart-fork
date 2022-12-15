4e38aa3fd082aedfa61957a7c22b3e6d4d17b45b
Shahar Levi <shaharLe88@gmail.com>
2022-12-15 17:35:20 +0200

    add data

---
+++ src/Data/data.js
@@ -0,0 +1,48 @@
+export let shoppingCart = [];
+
+export const groceries = [{
+        id: 1,
+        name: 'Meat',
+        quantity: Math.floor(Math.random() * 10),
+        price: 139.9
+    },
+    {
+        id: 2,
+        name: 'Milk',
+        quantity: Math.floor(Math.random() * 10),
+        price: 6.23
+    }, {
+        id: 3,
+        name: 'Sugar',
+        quantity: Math.floor(Math.random() * 10),
+        price: 4.6
+    },
+    {
+        id: 4,
+        name: 'Honey',
+        quantity: Math.floor(Math.random() * 10),
+        price: 42.9
+    }, {
+        id: 5,
+        name: 'Bread',
+        quantity: Math.floor(Math.random() * 10),
+        price: 13.5
+    },
+    {
+        id: 6,
+        name: 'Coffee',
+        quantity: Math.floor(Math.random() * 10),
+        price: 29.9
+    }, {
+        id: 7,
+        name: 'Rice',
+        quantity: Math.floor(Math.random() * 10),
+        price: 10.9
+    },
+    {
+        id: 8,
+        name: 'Bisli',
+        quantity: Math.floor(Math.random() * 10),
+        price: 4.4
+    }
+]
