f6366ce980078ebe2dcc58b801ff61adca212018
Shahar Levi <shaharLe88@gmail.com>
2022-12-15 17:51:37 +0200

    create listItem component, render data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import {List} from './Components/List'
-
+import { List, ListItem } from './components/List'
+import { groceries, shoppingCart } from './data/data'
 
 function App() {
   return (
     <div className="App">
       <div className="container-fluid mt-2">
         <div className="row">
-          <List container="col-md-6 col-sm-12" style="alert alert-primary" id="groceries" header="Groceries List" />
-          <List container="col-md-6 col-sm-12" style="alert alert-warning" id="shoppingCart" header="ShoppingCart" />
+          <List container="col-md-6 col-sm-12" style="alert alert-primary" id="groceries" header="Groceries List">
+            <ListItem data={groceries} name="groceries" />
+          </List>
+
+          <List container="col-md-6 col-sm-12" style="alert alert-warning" id="shoppingCart" header="ShoppingCart">
+            <ListItem data={shoppingCart} name="shoppingCart"/>
+          </List>
         </div>
       </div>
     </div>
--- src/Components/List.js
+++ src/Components/List.js
@@ -4,7 +4,30 @@ export function List(props) {
         <div className={props.style} role="alert">
             <ul className="list-group" id={props.id}>
                 <li className="list-group-item active" aria-current="true">{props.header}</li>
+                {props.children}
             </ul>
         </div>
     </div>
 }
+
+export function ListItem(props) {
+    const data = props.data;
+    if (props.name === 'groceries') {
+        return data.map((item, id) => {
+            return <li key={id} className="list-group-item d-flex justify-content-between align-items-center mt-4">
+                {item.name} - Stock: {item.quantity} - Price: {item.price}
+                <button type="button" id={`${item.id}`} className={`${item.quantity}` ? 'btn btn-primary' : 'btn btn-primary not-allowed'}>Add to Cart</button>
+            </li>
+        })
+    }
+
+    if (props.name === 'shoppingCart') {
+        return data.map((item, id) => {
+            return <li key={id} class="list-group-item d-flex justify-content-between" aria-current="true">
+                ${item.name} - Amount: ${item.quantity} - Total Price: ${item.price}
+                <button type="button" id={item.id} class="btn btn-primary">Remove 1</button>
+            </li>
+        })
+    }
+}
+
